99686f8e8e04757c2cf12c21bf73627c405a94ee
Eloi Matos <eloi_matos@hotmail.com>
2021-10-26 08:37:44 -0300

    using recycler view

--- app/src/main/java/com/example/testapplication/ui/listItems/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/ui/listItems/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.testapplication.ui.listItems
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testapplication.R
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,19 +16,24 @@ class MainActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable()
 
+    private var layoutManager: RecyclerView.LayoutManager? = null;
+    private var adapter: RecyclerAdapter? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        layoutManager = LinearLayoutManager(this)
+        recycler.layoutManager = layoutManager
 
         loadScreen(true)
 
         val catsSubscriber = mainViewModel.loadCats(5).subscribe(
             { list ->
-                val message = "Message = ${list[0].text}, Size = ${list.size}"
-               text1.text = message
+                adapter = RecyclerAdapter(list)
+                recycler.adapter = adapter
             }, //onNext
             { exception ->
-                text1.text = exception.message
+                //text1.text = exception.message
                 loadScreen(false)
             }, //onError
             {
@@ -38,8 +45,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadScreen(loading: Boolean){
-        text1.isVisible = !loading
-        text2.isVisible = !loading
+        //text1.isVisible = !loading
+        //text2.isVisible = !loading
         progressBar.isVisible = loading
     }
 
