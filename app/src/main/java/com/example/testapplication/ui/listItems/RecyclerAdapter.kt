d63011d737c92cdcc6e34c5e08668f4e27b1b682
Eloi Matos <eloi_matos@hotmail.com>
2021-11-06 15:20:12 -0300

    create item click listener

--- app/src/main/java/com/example/testapplication/ui/listItems/RecyclerAdapter.kt
+++ app/src/main/java/com/example/testapplication/ui/listItems/RecyclerAdapter.kt
@@ -10,7 +10,7 @@ import com.example.testapplication.R
 import com.example.testapplication.entity.model.CatData
 import kotlinx.android.synthetic.main.card_layout.view.*
 
-class RecyclerAdapter(private val list: List<CatData>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(private val list: List<CatData>, private val listner: OnItemClickListener) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
         return ViewHolder(view)
@@ -20,16 +20,32 @@ class RecyclerAdapter(private val list: List<CatData>) : RecyclerView.Adapter<Re
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
         with(holder) {
-            val context = holder.itemView.context
+            val context = itemView.context
             itemDetail.text = list[position].fact
             itemTitle.text = context.getString(R.string.fact_title)
             itemImage.setImageResource(R.drawable.image)
         }
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val itemImage: ImageView = itemView.item_image
         val itemTitle: TextView = itemView.item_title
         val itemDetail: TextView = itemView.item_detail
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val position: Int = adapterPosition
+
+            if (position != RecyclerView.NO_POSITION) {
+                listner.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
     }
 }
