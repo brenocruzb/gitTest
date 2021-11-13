9d118d88732759e6b874eed3c3c5c9941b7334e7
Eloi Matos <eloi_matos@hotmail.com>
2021-11-13 10:42:11 -0300

    create Favorites screen

---
+++ app/src/main/java/com/example/testapplication/ui/facts/favorites/FavoritesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testapplication.ui.facts.favorites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testapplication.R
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
