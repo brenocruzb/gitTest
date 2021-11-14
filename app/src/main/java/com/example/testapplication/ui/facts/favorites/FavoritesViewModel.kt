dc4c0d2028ac7f45ede7ce1fb99999376347e9ae
Eloi Matos <eloi_matos@hotmail.com>
2021-11-14 15:07:20 -0300

    refactoring FavoritesViewModel.kt

--- app/src/main/java/com/example/testapplication/ui/facts/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/testapplication/ui/facts/favorites/FavoritesViewModel.kt
@@ -2,7 +2,18 @@ package com.example.testapplication.ui.facts.favorites
 
 import androidx.lifecycle.ViewModel
 import com.example.testapplication.data.local.LocalFactManager
+import com.example.testapplication.entity.model.CatData
+import io.reactivex.Observable
 
 class FavoritesViewModel(private val localFacts: LocalFactManager): ViewModel() {
-    fun loadLocalCats(): List<String> = if (localFacts.hasFacts) localFacts.getFacts() else listOf<String>()
+    fun loadLocalCats(): Observable<List<String>> {
+        return Observable.create { emitter ->
+            if (localFacts.hasFacts) {
+                emitter.onNext(localFacts.getFacts())
+            } else {
+                emitter.onNext(listOf())
+            }
+            emitter.onComplete()
+        }
+    }
 }
